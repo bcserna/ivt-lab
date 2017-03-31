f8b4f91d0c0dfb783066f7a9dcb07a0097f2ff23
bcserna <cserna.balazs@gmail.com>
2017-03-31 02:18:15 -0700

    Fixed typo

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -81,7 +81,7 @@ public class GT4500 implements SpaceShip {
         //TODO implement feature
 	if (!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()) {
 		firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
-		wasPrimaryFired Last = true;
+		wasPrimaryFiredLast = true;
 	}
         break;
     }
